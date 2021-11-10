5e1b2a6185581c2e4952d0e0fe56facf4ef44aba
percyjxp <jp4131@columnbia.edu>
2021-11-09 23:56:10 -0500

    sign up

--- zappa/users/urls.py
+++ zappa/users/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
 
     path('login/', views.loginUser, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),
 
     path('', views.profiles, name="profiles"),
+
 ]
--- zappa/users/views.py
+++ zappa/users/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 # Create your views here.
 
 def loginUser(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -28,6 +30,28 @@ def loginUser(request):
             messages.error(request, 'Username or password is incorrect')
 
     return render(request, 'users/login_register.html')
+
+def registerUser(request):
+    page = 'register'
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created')
+
+            login(request, user)
+            return redirect('profiles')
+        
+        else:
+            messages.error(request, 'An error has occurred during registration!')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
