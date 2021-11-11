82b01768a16664079c54f6ea047c104c126837fa
amr2356 <81318928+amr2356@users.noreply.github.com>
2021-11-10 21:40:43 -0500

    Moved yfinance logic out of views and into models

--- zappa/api/views.py
+++ zappa/api/views.py
@@ -1,25 +1,26 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import GameSerializer, PortfolioSerializer, HoldingSerializer, TransactionSerializer
+from .serializers import GameSerializer, PortfolioSerializer, \
+                         HoldingSerializer, TransactionSerializer
 from trade_simulation.models import Game, Portfolio, Holding, Transaction
-from .stockdata import ask_Price, bid_Price
+
 
 @api_view(['GET'])
 def getRoutes(request):
 
     routes = [
-        {'GET':'/api/portfolios'},
-        {'GET':'/api/portfolio/id'},
-        {'POST':'/api/newportfolio'},
-        {'POST':'/api/buystock/id'},
-        {'POST':'/api/sellstock/id'},
-        {'GET':'/api/holdings'},
-        {'GET':'/api/holding/id'},
-        {'GET':'/api/transactions'},
-        {'GET':'/api/transaction/id'},
-        {'GET':'/api/games'},
-        {'GET':'/api/game/id'},
+        {'GET': '/api/portfolios'},
+        {'GET': '/api/portfolio/id'},
+        {'POST': '/api/newportfolio'},
+        {'POST': '/api/buystock/id'},
+        {'POST': '/api/sellstock/id'},
+        {'GET': '/api/holdings'},
+        {'GET': '/api/holding/id'},
+        {'GET': '/api/transactions'},
+        {'GET': '/api/transaction/id'},
+        {'GET': '/api/games'},
+        {'GET': '/api/game/id'},
     ]
 
     return Response(routes)
@@ -28,14 +29,19 @@ def getRoutes(request):
 @api_view(['GET'])
 def getPortfolios(request):
     portfolios = Portfolio.objects.all()
+    for portfolio in portfolios:
+        portfolio.computeTotalValue()
     serializer = PortfolioSerializer(portfolios, many=True)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def getPortfolio(request, pk):
-    portfolio = Portfolio.objects.get()
+    portfolio = Portfolio.objects.get(id=pk)
+    portfolio.computeTotalValue()
     serializer = PortfolioSerializer(portfolio, many=False)
     return Response(serializer.data)
+
 
 @api_view(['POST'])
 def makePortfolio(request):
@@ -48,77 +54,18 @@ def makePortfolio(request):
 
 @api_view(['POST'])
 def buyStock(request, pk):
-
     data = request.data
     portfolio = Portfolio.objects.get(id=pk)
-    askprice = ask_Price(data['ticker'])
-
-    #Code for updating holdings
-    holding, created = Holding.objects.get_or_create(
-        portfolio = portfolio,
-        ticker = data['ticker']
-    )
-
-    cost = askprice * float(data['shares'])
-    if float(portfolio.cash_balance) < cost:
-        print("Not enough cash to buy ${} of {}.".format(
-                    cost, data['ticker']))
-        return Response()
-
-    transaction = Transaction.objects.create()
-    transaction.portfolio = portfolio
-    transaction.ticker = data['ticker']
-    transaction.tradeType = "Buy"
-    transaction.shares = data['shares']
-    transaction.bought_price = askprice
-    transaction.save()
-
-    holding.shares = (float(0 if holding.shares is None else holding.shares)
-                      + float(data['shares']))
-    holding.save()
-
-    portfolio.cash_balance = float(portfolio.cash_balance) - cost
-    portfolio.save()
-
+    portfolio.buyHolding(data['ticker'], data['shares'])
     return Response()
 
 
 @api_view(['POST'])
 def sellStock(request, pk):
-
     data = request.data
     portfolio = Portfolio.objects.get(id=pk)
-    bidprice = bid_Price(data['ticker'])
-
-    #Code for updating holdings
-    holding, created = Holding.objects.get_or_create(
-        portfolio = portfolio,
-        ticker = data['ticker']
-    )
-
-    currentshares = float(0 if holding.shares is None else holding.shares)
-    if currentshares < float(data['shares']):
-        print("Not enough shares of {} to sell {}.".format(
-                        data['ticker'], float(data['shares'])))
-        return Response()
-
-    transaction = Transaction.objects.create()
-    transaction.portfolio = portfolio
-    transaction.ticker = data['ticker']
-    transaction.tradeType = "Sell"
-    transaction.shares = data['shares']
-    transaction.bought_price = bidprice
-    transaction.save()
-
-    holding.shares = currentshares - float(data['shares'])
-    holding.save()
-
-    portfolio.cash_balance = float(portfolio.cash_balance) + (bidprice * 
-                            float(data['shares']))
-    portfolio.save()
-
+    portfolio.sellHolding(data['ticker'], data['shares'])
     return Response()
-
 
 
 @api_view(['GET'])
@@ -127,11 +74,13 @@ def getHoldings(request):
     serializer = HoldingSerializer(holdings, many=True)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def getHolding(request, pk):
     holding = Holding.objects.get()
     serializer = HoldingSerializer(holding, many=False)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getTransactions(request):
@@ -139,17 +88,20 @@ def getTransactions(request):
     serializer = TransactionSerializer(transactions, many=True)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def getTransaction(request, pk):
     transaction = Transaction.objects.get()
     serializer = TransactionSerializer(transaction, many=False)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def getGames(request):
     games = Game.objects.all()
     serializer = GameSerializer(games, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getGame(request, pk):
