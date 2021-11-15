28389b798eede62aab04e19eaa799fbafbe5e410
Zach Brown <zachmb22@gmail.com>
2021-11-15 08:09:11 -0500

    Make comments on serializers, views, and utils

--- zappa/api/views.py
+++ zappa/api/views.py
@@ -22,7 +22,9 @@ DELETE_METHOD = "DELETE"
 
 @api_view(["GET"])
 def getRoutes(request):
-
+    """
+    Function that stores all the routes and gets the appropriate route
+    """
     routes = [
         {"GET": "/api/games"},
         {"GET": "/api/game/game_title"},
@@ -44,17 +46,25 @@ def getRoutes(request):
 
 @api_view(["GET"])
 def handle_games(request):
+    """
+    Function that handles getting all created games
+    """
     return Response(_get_game_standings_helper())
 
 
 @api_view(["GET", "POST", "DELETE"])
 def handle_game(request, game_title):
+    """
+    Function that handles getting, creating, or deleting one game.
+    """
+    # On a GET request show the requested game or throw an error
     if request.method == GET_METHOD:
         try:
             data = _get_game_helper(game_title)
             return Response(data)
         except Exception as e:
             return Response(status=500, data=e)
+    # On a POST request create the game or throw an error
     elif request.method == POST_METHOD:
         rules = request.data.get("rules")
         starting_balance = request.data.get("starting_balance")
@@ -63,6 +73,7 @@ def handle_game(request, game_title):
         except Exception as e:
             return Response(status=500, data=e)
         return Response()
+    # On a DELETE request delete the game or throw an error
     elif request.method == DELETE_METHOD:
         try:
             data = _delete_game_helper(game_title)
@@ -73,6 +84,9 @@ def handle_game(request, game_title):
 
 @api_view(["GET"])
 def handle_portfolios(request):
+    """
+    Function that handles getting all portfolios
+    """
     try:
         data = _get_portfolios_helper()
         return Response(data)
@@ -82,18 +96,24 @@ def handle_portfolios(request):
 
 @api_view(["GET", "POST", "DELETE"])
 def handle_portfolio(request, game_title, port_title):
+    """
+    Function that handles getting, creating, or deleting one portfolio.
+    """
+    # On a GET request show the requested portfolio or throw an error
     if request.method == GET_METHOD:
         try:
             data = _get_portfolio_helper(port_title, game_title)
             return Response(data)
         except Exception as e:
             return Response(status=500, data=e)
+    # On a POST request create the portfolio or throw an error
     elif request.method == POST_METHOD:
         try:
             _post_portfolio_helper(port_title, game_title)
             return Response()
         except Exception as e:
             return Response(status=500, data=e)
+    # On a DELETE request delete the portfolio or throw an error
     elif request.method == DELETE_METHOD:
         try:
             _delete_portfolio_helper(port_title, game_title)
@@ -104,6 +124,10 @@ def handle_portfolio(request, game_title, port_title):
 
 @api_view(["POST"])
 def trade(request):
+    """
+    Function that handles buying or selling stock. This will update the transaction, holdings table.
+    """
+    # If the trade type is a stock then set all the relevant data.
     securityType = request.data.get("securityType")
     if securityType == "stock":
         portfolio_title = request.data.get("portfolioTitle")
@@ -119,6 +143,9 @@ def trade(request):
 
 @api_view(["GET"])
 def handle_holdings(request):
+    """
+    Function that handles getting all active holdings.
+    """
     holdings = Holding.objects.all()
     serializer = HoldingSerializer(holdings, many=True)
     return Response(serializer.data)
@@ -126,11 +153,17 @@ def handle_holdings(request):
 
 @api_view(["GET"])
 def handle_holding(request, game_title, port_title, ticker):
+    """
+    Function that handles getting one holding within a portfolio
+    """
     return Response(_get_holding_helper(port_title, game_title, ticker))
 
 
 @api_view(["GET"])
 def handle_transactions(request):
+    """
+    Function that handles getting all transactions.
+    """
     transactions = Transaction.objects.all()
     serializer = TransactionSerializer(transactions, many=True)
     print(f"Successfully fetched all transactions: {serializer.data}")
@@ -139,6 +172,9 @@ def handle_transactions(request):
 
 @api_view(["GET"])
 def handle_transaction(request, pk):
+    """
+    Function that handles getting one particular transaction.
+    """
     transaction = Transaction.objects.get(uid=pk)
     serializer = TransactionSerializer(transaction, many=False)
     print(f"Successfully fetched transaction: {serializer.data}")
