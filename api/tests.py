58e800b0b43cdb8b26be868497c93f2f8b2f087c
arunamatata <arunamatata@hotmail.com>
2021-12-05 08:15:55 -0500

    Added more tests for models

--- api/tests.py
+++ api/tests.py
@@ -15,7 +15,9 @@ from api.helpers import (
     _delete_portfolio_helper,
     _post_portfolio_helper,
     _trade_stock_helper,
+    _trade_option_helper,
     _get_holding_helper,
+    _get_option_helper,
 )
 from api.views import (
     handle_games,
@@ -25,10 +27,12 @@ from api.views import (
     trade,
     handle_holdings,
     handle_holding,
+    handle_options,
+    handle_option,
     handle_transactions,
     handle_transaction,
 )
-from .utils import find_game_by_title, find_portfolio, find_holding
+from .utils import find_game_by_title, find_portfolio, find_holding, find_option
 
 
 class HelperTestCase(TestCase):
@@ -225,7 +229,7 @@ class HelperTestCase(TestCase):
 
     def test_delete_portfolio_not_exist(self):
         """
-        Test that deleting a portfolio that does not exit throws an exception
+        Test that deleting a portfolio that does not exist throws an exception
         """
         # GIVEN
         game_title = "Game Title"
@@ -235,7 +239,31 @@ class HelperTestCase(TestCase):
         with self.assertRaises(Exception):
             _delete_portfolio_helper(portfolio_title, game_title)
 
-    @mock.patch("api.helpers._trade_stock_helper", return_value=None)
+    def test_create_portfolio_game_not_exist(self):
+        """
+        Test that creating a portfolio in a game that does not exist throws an exception
+        """
+        # GIVEN
+        game_title = "Game Title"
+        portfolio_title = "Portfolio Title"
+        # WHEN / THEN
+        with self.assertRaises(Exception):
+            _post_portfolio_helper(portfolio_title, game_title, self.test_user.username)
+
+    def test_create_portfolio_already_exists(self):
+        """
+        Test that creating a portfolio in a game that already has a portfolio of that name throws an exception
+        """
+        # GIVEN
+        game_title = "Game Title"
+        portfolio_title = "Portfolio Title"
+        _create_game_helper(game_title, "test rules", 10000)
+        _post_portfolio_helper(portfolio_title, game_title, self.test_user.username)
+        # WHEN / THEN
+        with self.assertRaises(Exception):
+            _post_portfolio_helper(portfolio_title, game_title, self.test_user.username)
+
+    @mock.patch("trade_simulation.models.Holding.ask_price", return_value=200)
     def test_trade_stock_helper_success_buy(self, mock_buy_stock):
         """
         Test that we buy a stock successfully if shares > 0
@@ -253,7 +281,29 @@ class HelperTestCase(TestCase):
         holding = find_holding(portfolio_title, game_title, ticker)
         assert holding.shares == shares
 
-    @mock.patch("api.helpers._trade_stock_helper")
+    @mock.patch("trade_simulation.models.Option.ask_price", return_value=2000)
+    @mock.patch("trade_simulation.models.Holding.ask_price", return_value=200)
+    def test_trade_stock_helper_option_success_buy(self, mock_buy_stock, mock_buy_option):
+        """
+        Test that we buy a stock while exercising option successfully if shares > 0
+        """
+        # GIVEN
+        game_title = "Game Title"
+        portfolio_title = "Portfolio Title"
+        _create_game_helper(game_title, "test rules", 10000)
+        _post_portfolio_helper(portfolio_title, game_title, self.test_user.username)
+        ticker = "AAPL"
+        shares = 3
+        contract = "AAPL211223C00148000"
+        quantity = 1
+        # WHEN
+        _trade_option_helper(portfolio_title, game_title, contract, quantity)
+        _trade_stock_helper(portfolio_title, game_title, ticker, shares, exercise=contract)
+        # THEN
+        holding = find_holding(portfolio_title, game_title, ticker)
+        assert holding.shares == shares
+
+    @mock.patch("trade_simulation.models.Holding.bid_price", return_value=200)
     def test_trade_stock_helper_success_sell(self, mock_sell_stock):
         """
         Test that we sell a stock successfully if shares < 0
@@ -272,6 +322,29 @@ class HelperTestCase(TestCase):
         holding = find_holding(portfolio_title, game_title, ticker)
         assert holding.shares == 1
 
+    @mock.patch("trade_simulation.models.Option.ask_price", return_value=9000)
+    @mock.patch("trade_simulation.models.Holding.bid_price", return_value=200)
+    def test_trade_stock_helper_option_success_sell(self, mock_sell_stock, mock_buy_option):
+        """
+        Test that we sell a stock while exercising option successfully if shares < 0
+        """
+        # GIVEN
+        game_title = "Game Title"
+        portfolio_title = "Portfolio Title"
+        _create_game_helper(game_title, "test rules", 30000)
+        _post_portfolio_helper(portfolio_title, game_title, self.test_user.username)
+        ticker = "TSLA"
+        shares = 2
+        contract = "TSLA211231P01115000"
+        quantity = 1
+        _trade_stock_helper(portfolio_title, game_title, ticker, shares + 1)
+        _trade_option_helper(portfolio_title, game_title, contract, quantity)
+        # WHEN
+        _trade_stock_helper(portfolio_title, game_title, ticker, -shares, exercise=contract)
+        # THEN
+        holding = find_holding(portfolio_title, game_title, ticker)
+        assert holding.shares == 1
+
     def test_trade_stock_helper_error(self):
         """
         Test that we cannot buy or sell a stock if portfolio does not exist
@@ -284,39 +357,54 @@ class HelperTestCase(TestCase):
         with self.assertRaises(Exception):
             _trade_stock_helper(portfolio_title, game_title, "AAPL", 3)
 
-    @mock.patch("trade_simulation.models.Portfolio.buy_holding")
-    def test_buy_stock_helper_success(self, buy_mock):
+    @mock.patch("api.helpers._trade_option_helper", return_value=None)
+    def test_trade_option_helper_success_buy(self, mock_buy_option):
         """
-        Test that buy stock helper works successfully
+        Test that we buy an option successfully if quantity > 0
         """
         # GIVEN
         game_title = "Game Title"
         portfolio_title = "Portfolio Title"
         _create_game_helper(game_title, "test rules", 10000)
         _post_portfolio_helper(portfolio_title, game_title, self.test_user.username)
-        ticker = "AAPL"
-        shares = 3
+        contract = "AAPL211223C00148000"
+        quantity = 1.0
         # WHEN
-        _trade_stock_helper(portfolio_title, game_title, ticker, shares)
+        _trade_option_helper(portfolio_title, game_title, contract, quantity)
         # THEN
-        buy_mock.assert_called_with(ticker, shares, exercise=None)
+        option = find_option(portfolio_title, game_title, contract)
+        assert option.quantity == quantity
 
-    @mock.patch("trade_simulation.models.Portfolio.sell_holding")
-    def test_sell_stock_helper_success(self, sell_mock):
+    @mock.patch("api.helpers._trade_option_helper")
+    def test_trade_option_helper_success_sell(self, mock_sell_option):
         """
-        Test that sell stock helper works successfully
+        Test that we sell an option successfully if quantity < 0
         """
         # GIVEN
         game_title = "Game Title"
         portfolio_title = "Portfolio Title"
         _create_game_helper(game_title, "test rules", 10000)
         _post_portfolio_helper(portfolio_title, game_title, self.test_user.username)
-        ticker = "AAPL"
-        shares = -3
+        contract = "AAPL211223C00148000"
+        quantity = 1.0
+        _trade_option_helper(portfolio_title, game_title, contract, quantity + 1)
         # WHEN
-        _trade_stock_helper(portfolio_title, game_title, ticker, shares)
+        _trade_option_helper(portfolio_title, game_title, contract, -quantity)
         # THEN
-        sell_mock.assert_called_with(ticker, -shares, exercise=None)
+        option = find_option(portfolio_title, game_title, contract)
+        assert option.quantity == 1
+
+    def test_trade_option_helper_error(self):
+        """
+        Test that we cannot buy or sell an option if portfolio does not exist
+        """
+        # GIVEN
+        game_title = "Game Title"
+        portfolio_title = "Portfolio Title"
+        _create_game_helper(game_title, "test rules", 10000)
+        # WHEN / THEN
+        with self.assertRaises(Exception):
+            _trade_option_helper(portfolio_title, game_title, "AAPL211223C00148000", 1.0)
 
     def test_get_holding_helper_success(self):
         """
@@ -346,6 +434,35 @@ class HelperTestCase(TestCase):
         # WHEN / THEN
         with self.assertRaises(Exception):
             _get_holding_helper(portfolio_title, game_title, "AAPL")
+
+    def test_get_option_helper_success(self):
+        """
+        Test that we can successfully get an option in a portfolio
+        """
+        # GIVEN
+        game_title = "Game Title"
+        portfolio_title = "Portfolio Title"
+        _create_game_helper(game_title, "test rules", 10000)
+        _post_portfolio_helper(portfolio_title, game_title, self.test_user.username)
+        contract = "AAPL211223C00148000"
+        quantity = 1.0
+        _trade_option_helper(portfolio_title, game_title, contract, quantity)
+        # WHEN
+        data = _get_option_helper(portfolio_title, game_title, contract)
+        # THEN
+        assert data["contract"] == contract
+
+    def test_get_option_helper_error_not_exist(self):
+        """
+        Test that throws an error if portfolio does not exist
+        """
+        # GIVEN
+        game_title = "Game Title"
+        portfolio_title = "Portfolio Title"
+        _create_game_helper(game_title, "test rules", 10000)
+        # WHEN / THEN
+        with self.assertRaises(Exception):
+            _get_option_helper(portfolio_title, game_title, "AAPL211223C00148000")
 
     def test_handle_games_success(self):
         """
@@ -524,12 +641,12 @@ class HelperTestCase(TestCase):
 
     def test_handle_holding_success(self):
         """
-        Test that we can get a holding successfully successfully
+        Test that we can get a holding successfully
         """
         # GIVEN
         request = HttpRequest()
         request.method = 'GET'
-        request.path = '/api/holdings/'
+        request.path = '/api/holding/'
         game_title = "Game Title"
         portfolio_title = "Portfolio Title"
         _create_game_helper(game_title, "test rules", 10000)
@@ -539,6 +656,74 @@ class HelperTestCase(TestCase):
         _trade_stock_helper(portfolio_title, game_title, ticker, shares)
         # WHEN / THEN
         self.assertEqual(handle_holding(request, game_title, portfolio_title, ticker).status_code, 200)
+
+    def test_handle_holding_not_found(self):
+        """
+        Test that a holding not found raises an exception
+        """
+        # GIVEN
+        request = HttpRequest()
+        request.method = 'GET'
+        request.path = '/api/holding/'
+        game_title = "Game Title"
+        portfolio_title = "Portfolio Title"
+        _create_game_helper(game_title, "test rules", 10000)
+        _post_portfolio_helper(portfolio_title, game_title, self.test_user.username)
+        ticker = "AAPL"
+        # WHEN / THEN
+        self.assertEqual(handle_holding(request, game_title, portfolio_title, ticker).status_code, 500)
+
+    def test_handle_options_success(self):
+        """
+        Test that we can get all options successfully
+        """
+        # GIVEN
+        request = HttpRequest()
+        request.method = 'GET'
+        request.path = '/api/options/'
+        game_title = "Game Title"
+        portfolio_title = "Portfolio Title"
+        _create_game_helper(game_title, "test rules", 10000)
+        _post_portfolio_helper(portfolio_title, game_title, self.test_user.username)
+        contract = "AAPL211223C00148000"
+        quantity = 4.0
+        _trade_option_helper(portfolio_title, game_title, contract, quantity)
+        # WHEN / THEN
+        self.assertEqual(handle_options(request).status_code, 200)
+
+    def test_handle_option_success(self):
+        """
+        Test that we can get an option successfully
+        """
+        # GIVEN
+        request = HttpRequest()
+        request.method = 'GET'
+        request.path = '/api/holdings/'
+        game_title = "Game Title"
+        portfolio_title = "Portfolio Title"
+        _create_game_helper(game_title, "test rules", 10000)
+        _post_portfolio_helper(portfolio_title, game_title, self.test_user.username)
+        contract = "AAPL211223C00148000"
+        quantity = 4.0
+        _trade_option_helper(portfolio_title, game_title, contract, quantity)
+        # WHEN / THEN
+        self.assertEqual(handle_option(request, game_title, portfolio_title, contract).status_code, 200)
+
+    def test_handle_option_not_found(self):
+        """
+        Test that an option not found raises an exception
+        """
+        # GIVEN
+        request = HttpRequest()
+        request.method = 'GET'
+        request.path = '/api/holdings/'
+        game_title = "Game Title"
+        portfolio_title = "Portfolio Title"
+        _create_game_helper(game_title, "test rules", 10000)
+        _post_portfolio_helper(portfolio_title, game_title, self.test_user.username)
+        contract = "AAPL211223C00148000"
+        # WHEN / THEN
+        self.assertEqual(handle_option(request, game_title, portfolio_title, contract).status_code, 500)
 
     def test_handle_transactions_success(self):
         """
