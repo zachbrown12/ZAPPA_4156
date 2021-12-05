89900a008618134d4bb7c5357a2753ef5a1b41e3
ppanwin10 <jp4131@columbia.edu>
2021-12-04 23:28:28 -0800

    refined pages and add portfolio

--- trade_simulation/urls.py
+++ trade_simulation/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.games, name="games"),
     path("games", views.games, name="games"),
-    path('game/<str:title>/', views.game, name="game"),
+    path('game/<str:pk>/', views.game, name="game"),
+    path('portfolio/<str:pk>/', views.portfolio, name="portfolio"),
 ]
