7d5a88b0ad08461c7008260bf7da64686dd3d7d2
azepecki <annezepecki@berkeley.edu>
2021-12-09 22:36:12 -0800

    remove comments

--- reactfrontend/src/components/landingPage.js
+++ reactfrontend/src/components/landingPage.js
@@ -1,44 +1,46 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
 import axios from "axios";
-import GameTable from './gameTable';
-import NewGameDialog from './newGameDialog';
+import GameTable from "./gameTable";
+import NewGameDialog from "./newGameDialog";
 
 export default function LandingPage() {
-    // TODO: Create newGameDialog component
-  // Allows a user to specify the name of a game
-  // eslint-disable-next-line
   const [newGameDialogVisible, setNewGameDialogVisible] = useState(false);
-
-  // eslint-disable-next-line
   let [gamesData, setGamesData] = useState([]);
   const setVisible = () => {
-      setNewGameDialogVisible(!newGameDialogVisible);
-  }
+    setNewGameDialogVisible(!newGameDialogVisible);
+  };
 
-
-  // eslint-disable-next-line
   const fetchGameData = async () => {
     axios
-    .get('/api/games')
-    .then((resp) => {
-        console.log(resp.data)
+      .get("/api/games")
+      .then((resp) => {
+        console.log(resp.data);
         setGamesData(resp.data);
-    }).catch((err) => console.log(err));
-  }
+      })
+      .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
-      (async () => {
-        await fetchGameData();
-      })();
+    (async () => {
+      await fetchGameData();
+    })();
   }, []);
   return (
     <div>
-      <Button variant="contained" onClick={setVisible}>Create New Game</Button>
+      <Button variant="contained" onClick={setVisible}>
+        Create New Game
+      </Button>
       <GameTable data={gamesData}></GameTable>
-      {newGameDialogVisible ? <NewGameDialog open={newGameDialogVisible} setDialogVisible={setVisible}></NewGameDialog>: <></>}
+      {newGameDialogVisible ? (
+        <NewGameDialog
+          open={newGameDialogVisible}
+          setDialogVisible={setVisible}
+        ></NewGameDialog>
+      ) : (
+        <></>
+      )}
     </div>
-
   );
 }
